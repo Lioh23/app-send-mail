aaf31eff8c002ff9470b944fd7effe06bb76c78e
Lioh23 <60784631+Lioh23@users.noreply.github.com>
2021-04-05 20:03:38 -0300

    Update processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -59,13 +59,13 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'aurelin95@gmail.com';                     //SMTP username
-    $mail->Password   = 'bloodintheeyes';                               //SMTP password
+    $mail->Username   = 'seuemail@gmail.com';                     //SMTP username
+    $mail->Password   = 'suasenha';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('aurelin95@gmail.com', 'Testando envio de mensagem');
+    $mail->setFrom('seuemail@gmail.com', 'Testando envio de mensagem');
     $mail->addAddress($mensagem->para);     //Add a recipient
     // $mail->addReplyTo('aurelin95@gmail.com', 'Information');
     // $mail->addCC('cc@example.com');
